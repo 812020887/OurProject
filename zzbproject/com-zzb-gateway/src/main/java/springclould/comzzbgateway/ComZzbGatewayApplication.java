46c6302fa67191110849e6e8c48907c847c2fd2d
e <812020887@qq.com>
2019-01-03 17:35:36 +0800

    gateway

---
+++ zzbproject/com-zzb-gateway/src/main/java/springclould/comzzbgateway/ComZzbGatewayApplication.java
@@ -0,0 +1,14 @@
+package springclould.comzzbgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComZzbGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComZzbGatewayApplication.class, args);
+    }
+
+}
+
